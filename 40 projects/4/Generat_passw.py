f1b34dd0267a9fc97e77f80d6a79796272e6153e
urdrops <116820811+urdrops@users.noreply.github.com>
2023-02-09 11:38:19 +0500

    git commit

--- 40 projects/4/Generat_passw.py
+++ 40 projects/4/Generat_passw.py
@@ -1,8 +1,8 @@
 import random as r
 import string as s
 punctuation, digits, letters_low, letter_up = s.punctuation, s.digits, s.ascii_lowercase, s.ascii_uppercase
-no = 'no'
-while no == 'no':
+turn_off = 'no'
+while turn_off == 'no':
     user = int(input('длина пароля: \n'))
     set_up_digits = input('хотите добавить числа в пароль? yes/no \n')
     set_up_letters = input('хотите добавить ,буквы в пароль? yes/no \n')
@@ -20,5 +20,5 @@ while no == 'no':
     for i in range(user):
         a += str(r.choice(sort(set_up_digits, set_up_letters, set_up_punctuation) ))
     print(a)
-    no = input('согласны на такой пароль no/yes ? \n')
+    turn_off = input('согласны на такой пароль no/yes ? \n')
 print('done!')
