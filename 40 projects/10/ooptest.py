06de829f907b7c61723a84225077c3596a8902e1
urdrops <116820811+urdrops@users.noreply.github.com>
2023-06-18 03:12:17 +0500

    s

--- 40 projects/10/ooptest.py
+++ 40 projects/10/ooptest.py
@@ -1,12 +1,30 @@
-class Teacher():
-    def __init__(self,name,age,degree,iq):
-        self.age = age 
-        self.degree = degree 
-        self.iq = iq
-        self.name = name
-    def Hello(name,age):
-        print(f"today we gonna teach and i am {name} and my age is {age}")
-    def Teach(degree):
-        print(f"i am ready to teach you with {degree} degree")
-Teacher = Teacher()
-        
+class Person:
+    def __init__(self,name,age):
+            self.name = name
+            self.age = age
+            self.__iq = 160
+    @property
+    def get_iq(self):
+          return self.__iq
+    
+    @get_iq.setter
+    def set_iq(self,nwiq):
+          self.__iq = nwiq
+class Physics(Person):
+      def create_bomb(self):
+            print(self.name,"made bomb")
+class Chemestry(Person):
+      def create_poison(self):
+           print(self.name,"made poison") 
+class Genius(Physics,Chemestry):
+      pass 
+person = Person("jahon",18)  
+Jahon = Physics()
+alisher = Chemestry("alisher",19)
+Jahon.create_bomb()
+alisher.create_poison()
+Baxti = Genius("baxti",18)
+Baxti.create_bomb()
+Baxti.create_poison()
+
+print(Baxti.get_iq())
