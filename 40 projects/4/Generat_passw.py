e0388590fadea54fafbac243ed535555dccfc758
urdrops <116820811+urdrops@users.noreply.github.com>
2023-06-19 16:23:11 +0500

    upd

--- 40 projects/4/Generat_passw.py
+++ 40 projects/4/Generat_passw.py
@@ -1,20 +1,23 @@
 import random as r
 import string as s
-punctuation, digits, letters_low, letter_up = s.punctuation, s.digits, s.ascii_lowercase, s.ascii_uppercase
+
 turn_off = 'no'
 while turn_off == 'no':
+
     user = int(input('длина пароля: \n'))
+
     set_up_digits = input('хотите добавить числа в пароль? yes/no \n')
     set_up_letters = input('хотите добавить ,буквы в пароль? yes/no \n')
     set_up_punctuation = input('хотите добавить пунктуации в пароль? yes/no \n')
+    
     def sort (a,b,c):
         full = ''
         if a == 'yes':
-            full += digits
+            full += s.digits
         if b == 'yes':
-            full += letter_up + letters_low
+            full += s.ascii_uppercase + s.ascii_lowercase
         if c == 'yes':
-            full += punctuation
+            full += s.punctuation
         return full
     a = ''
     for i in range(user):
